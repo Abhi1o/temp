7f07d721f1f3dbd57225fef40905d1995406c5ac
abhi <abhisheksing147@gmail.com>
2023-02-16 17:21:27 +0530

    today is second day i have worked on variable and its print type

---
+++ javaScript/variable.js
@@ -0,0 +1,13 @@
+// variables in JavaScript
+// string
+var name = "abhishek";  //declaration
+console.log(name);
+
+//Number
+var num = 32;
+console.log(num);
+
+// Boolean
+var isLight = false;
+console.log(isLight);
+
---
+++ javaScript/variable2.js
@@ -0,0 +1,15 @@
+const userId = "abhi147";
+var firstName = "abhisek";
+var lastName = "kumar";
+var email = " abhisingh147@gamil.com";
+var password = 9024273933;
+var contact_no = 1245789632;
+var country = "India";
+var state = "Bihar";
+
+var isLoggedInFromGoogle = true;
+var isLoggedInFromFacebook = true;
+var isLoggedInFromLinkedIn = false; 
+console.log(`
+    His unique Id is ${userId} person first name is  ${firstName} and last name is ${lastName} email Id is ${email} password is ${password} contact number is ${contact_no} he's from ${state} in ${country}.
+`)
